18ae174c25a048b3213a2a7912199b05be811e8a
pjusto <pjusto@cge.rj.gov.br>
2025-07-08 20:59:39 -0300

    hotfix

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { injectAnalytics } from '@vercel/analytics/sveltekit'
+import { inject } from "@vercel/analytics"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {injectAnalytics()}
+    {inject()}
     <App />
   </StrictMode>,
 )
