20605b95f0b71e21261d5b7c670173cbdef1004f
pjusto <pjusto@cge.rj.gov.br>
2025-07-08 20:57:18 -0300

    analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { injectAnalytics } from '@vercel/analytics/sveltekit'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    {injectAnalytics()}
     <App />
   </StrictMode>,
 )
